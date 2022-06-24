420ab29c1df2ef5af3acfb0b781cd5ace7f3d796
Sangkwun <wer2774@gmail.com>
2022-06-24 19:53:59 +0900

    Simplify buildStyles

--- packages/benchmarks/emotion/over-bridge/client/utils.js
+++ packages/benchmarks/emotion/over-bridge/client/utils.js
@@ -105,73 +105,11 @@ const ComponentCSSDeclarations = [
   /* TODO: Add properties as many as possible... e.g. grid, animation, ... */
 ];
 
-export function buildCSSStyles(
+export function buildStyles(
   // theme,
   props
 ) {
   const cssProps = pick(props, ComponentCSSDeclarations);
   // applyColorTheme(theme, cssProps);
   return css(cssProps);
-}
-
-// export function buildBreakpointStyles(
-//   theme,
-//   props
-// ) {
-//   const styles = Object.entries(props)
-//     .sort(([bpA, _sA], [bpB, _sB]) => {
-//       const valueA = isBreakpoint(bpA) ? theme.breakpoint.breakpoints[bpA] : Number(bpA);
-//       const valueB = isBreakpoint(bpB) ? theme.breakpoint.breakpoints[bpB] : Number(bpB);
-//       return valueB - valueA;
-//     })
-//     .map(([bp, s]) => s && buildBaseStyles(theme, s, bp));
-//   return css(styles);
-// }
-
-// export function buildTypographyStyles(typography) {
-//   if (!typography) return undefined;
-//   if (isTypography(typography)) return theme.typography[typography];
-
-//   const styles =
-//     typography &&
-//     Object.entries(typography).map(
-//       ([bp, t]) => t && buildMediaQuery(theme, theme.typography[t as Typography], bp as Breakpoint)
-//     );
-
-//   return css(styles);
-// }
-
-export function buildBaseStyles(
-  // theme,
-  props
-  // bp?
-) {
-  // const typographyStyles = buildTypographyStyles(props.typography);
-  const baseStyles = buildCSSStyles(props);
-
-  // const serializedStyles = css(typographyStyles, baseStyles);
-
-  return baseStyles;
-}
-
-// export function buildResponsiveStyles(
-//   theme,
-//   props,
-// ) {
-//   const pickedProps = pick(props, ReservedBreakpoint, "customResponsive");
-
-//   const { customResponsive, ...otherProps } = pickedProps;
-//   const baseStyles =
-//     otherProps && buildBreakpointStyles(theme, { ...otherProps, ...customResponsive });
-
-//   return css(baseStyles);
-// }
-
-export function buildStyles(
-  // theme,
-  props
-) {
-  const baseStyles = buildBaseStyles(props);
-  // const responsiveStyles = buildResponsiveStyles(theme, props);
-  return baseStyles;
 }
