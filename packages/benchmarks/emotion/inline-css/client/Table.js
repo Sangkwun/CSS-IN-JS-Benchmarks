1cbe9ac1d667b8fc9576380a3cf9c4fd70167e1b
Sangkwun <wer2774@gmail.com>
2022-06-28 16:59:53 +0900

    Fix webpack config

--- packages/benchmarks/emotion/inline-css/client/Table.js
+++ packages/benchmarks/emotion/inline-css/client/Table.js
@@ -4,21 +4,21 @@ import { css } from '@emotion/react';
 
 const Table = ({ table, toPercent }) => (
   <div
-    className={css`
+    css={css`
       display: table;
       margin-top: 10px;
   `}
   >
     {table.map((row, i) => (
       <div
-        className={css`
+        css={css`
         display: table-row;
         `}
         key={i}
       >
         {row.map((x, j) => (
           <div
-            className={css`
+            css={css`
               display: table-cell;
               padding: 10px;
               background: rgba(74, 174, 53, ${x});
