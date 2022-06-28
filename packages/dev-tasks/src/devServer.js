d1efe6992b5b23a0210ba06eb3c001a73bca7475
Sangkwun <wer2774@gmail.com>
2022-06-28 14:14:42 +0900

    Fix devServer

--- packages/dev-tasks/src/devServer.js
+++ packages/dev-tasks/src/devServer.js
@@ -1,13 +1,19 @@
 #!/usr/bin/env node
+// process.env.NODE_ENV = 'development';
 const webpackDevServer = require('webpack-dev-server');
 const webpack = require('webpack');
 
 const webpackConfig = require('./webpack.config.js');
 const compiler = webpack(webpackConfig);
 
-const server = new webpackDevServer(compiler, {
-  quiet: true,
-  stats: 'errors-only',
-});
+const server = new webpackDevServer(
+  {
+    hot: false,
+    client: {
+      logging: 'error',
+    },
+  },
+  compiler
+);
 
 server.listen(3000);
